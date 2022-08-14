7bf720ab710eb154eb72b599c146ba0d0489f3f8
Okan Coşkun <okancoskun@192.168.1.51>
2022-08-14 19:21:07 +0300

    ilk commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***** while examples ******");
+        Console.WriteLine("Lutfen Ortalamasini Istediginiz Sayiyi Giriniz: ");
+        int sayi = int.Parse(Console.ReadLine().Trim());
+        int sayac =1; //1'den baslayarak girilen sayiya kadar olan sayilarin ortalamasi icin
+        int toplam =0;
+        while (sayac <= sayi)
+        {
+            toplam += sayac;
+            sayac++;
+        }
+        Console.WriteLine("Girdiginiz sayinin ortalamasi: "+toplam/sayi);
+
+        char chr = 'a';
+        while (chr <= 'z')
+        {
+            Console.Write(chr);
+            chr++;
+        }
+
+        string[] cars = {"BMW","Audi","Volkswagen","Mercedes"};
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car);
+        }
+
+    }
+}
